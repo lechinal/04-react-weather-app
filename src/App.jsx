48d3dc38fcfb4b5e98ad914e95cd8434fc283af3
lechinal <alin.lechintan@outlook.com>
2023-09-28 10:02:38 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const api = {
   key: '8d4cdf8f8a1e4cb1a3f8e886d69e86c3',
-  base: 'http://api.openweathermap.org/data/2.5/',
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 
 function App() {
